41f33de2d535d75789df35d7e333df6f59d7394b
Moneebah <moneebah.py@gmail.com>
2023-11-18 14:22:45 +0500

    Create 1SignUp.py

---
+++ pages/1SignUp.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from utils import hash_password, create_new_tenant, verify_tenant
+
+def show_signup():
+    st.subheader("Personalize 🍏")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+    if st.button("SignUp"):
+        hashed_password = hash_password(password)
+        tenant_name = f"{username}_{hashed_password}"[:64]
+        if verify_tenant(tenant_name):
+            st.session_state['user'] = username
+            st.session_state['tenant_name'] = tenant_name  # Store tenant name
+            st.success("Logged in successfully! Lets chat!")
+        else:
+            create_new_tenant(tenant_name)
+            st.session_state['user'] = username
+            st.session_state['tenant_name'] = tenant_name  # Store tenant name
+
+            # Create new tenant logic..
+            st.success("Signup successful! Lets get to know you then chat!")
+
+# Display the signup page
+show_signup()
