f1e196b5822cd0e0ff563c4b68044e89b7c9198a
Moneebah <moneebah.py@gmail.com>
2023-11-18 14:23:11 +0500

    Add files via upload

---
+++ pages/2Form.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+def show_health_info_form():
+    st.title("Health Information Form")
+    st.write("Please enter your health details to personalize your experience.")
+    create_health_info_form()
+
+def create_health_info_form():
+    with st.form(key='health_info_form'):
+        # Personal details
+        age = st.number_input("Age", min_value=0, max_value=120, step=1)
+        gender = st.selectbox("Gender", ["Male", "Female", "Prefer not to say"])
+        weight = st.number_input("Weight (kg)", min_value=20, max_value=200, step=1)
+        height = st.number_input("Height (cm)", min_value=100, max_value=250, step=1)
+
+        # Health details
+        allergies = st.multiselect("Allergies", ["Nuts", "Dairy", "Gluten", "Seafood", "Pollen", "Latex", "No known allergies"], default=[])
+        allergies_other = st.text_input("Other Allergies")
+
+        diseases = st.multiselect("Diseases", ["Diabetes", "Hypertension", "Heart Disease", "Asthma", "Thyroid Disorder", "None"], default=[])
+        diseases_other = st.text_input("Other Diseases")
+
+        health_goals = st.multiselect("Health Goals", ["Gain Weight", "Maintain Weight", "Lose Weight", "Improve Muscle Tone", "Increase Stamina/Endurance", "Improve Overall Health"], default=[])
+        health_goals_other = st.text_input("Other Health Goals")
+
+        activity_level = st.selectbox("Activity Level", ["Sedentary (little or no exercise)", "Lightly Active (light exercise or sports 1-3 days a week)", "Moderately Active (moderate exercise or sports 3-5 days a week)", "Very Active (hard exercise or sports 6-7 days a week)", "Extra Active (very hard exercise, physical job or training twice a day)"])
+
+        dietary_preferences = st.multiselect("Dietary Preferences", ["Vegetarian", "Vegan", "Pescatarian", "Omnivore", "Keto", "Paleo", "None"], default=[])
+        dietary_preferences_other = st.text_input("Other Dietary Preferences")
+
+        app_purpose = st.multiselect("Purpose of Using Our App", ["Fitness Tracking", "Health Monitoring", "Diet/Nutrition Planning", "Weight Management", "Medical Condition Management", "Physical Activity Motivation", "Research Purposes"], default=[])
+        app_purpose_other = st.text_input("Other Purpose of Using App")
+
+        submit_button = st.form_submit_button(label='Submit')
+
+        if submit_button:
+            # Encapsulating responses in a dictionary
+            form_responses = {
+                "Age": str(age),
+                "Gender": str(gender),
+                "Weight": str(weight),
+                "Height": str(height),
+                "Allergies": allergies + ([allergies_other] if allergies_other else []),
+                "Diseases": diseases + ([diseases_other] if diseases_other else []),
+                "Health Goals": health_goals + ([health_goals_other] if health_goals_other else []),
+                "Activity Level": activity_level,
+                "Dietary Preferences": dietary_preferences + ([dietary_preferences_other] if dietary_preferences_other else []),
+                "Purpose of Using App": app_purpose + ([app_purpose_other] if app_purpose_other else [])
+            }
+            
+            
+            
+show_health_info_form()
---
+++ pages/3Upload.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from utils import upload_document, show_user_documents_screen ,batch_import_documents
+
+def show_upload_screen():
+    st.subheader("Upload Your Medical Documents")
+    uploaded_file = st.file_uploader("Choose a file", type=['pdf', 'docx', 'txt', 'png', 'jpg', 'jpeg'])
+    if uploaded_file is not None:
+        user_id = st.session_state.tenant_name  # Retrieve tenant name
+        if user_id:
+            with st.spinner("Processing document..."):
+                try:
+                    data_objects = upload_document(uploaded_file, user_id)
+                    batch_import_documents(data_objects, user_id)
+                    st.success("File uploaded and processed successfully!")
+                    st.write("Here are your documents:")
+                    show_user_documents_screen(user_id)
+
+                except Exception as e:
+                    st.error(f"An error occurred: {e}")
+        else:
+            st.error("Tenant ID is not set. Please login first.")
+            
+show_upload_screen()
+
