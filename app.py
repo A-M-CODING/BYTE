e26d51055a90195143c6dd2588319c2af85ed2f2
Moneebah <moneebah.py@gmail.com>
2023-11-22 13:31:23 +0500

    guide for new users app.py

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ st.set_page_config(
 st.title("Welcome to BYTE 🍏")
 st.markdown("### Your Gateway to Personalized Health and Nutrition")
 
+# Instructions for New Users
+st.markdown("## Getting Started Guide for New Users")
+st.markdown("""
+Please Follow these steps to get the best experience:
+1. **Sign Up**: Visit the Signup Page. If you encounter any errors, please try clicking the 'Sign Up' button again, or refresh the page and attempt to sign up once more.
+2. **Fill Out the Form**: After signing up, move to the form page to complete the Health Information Form with your details.
+3. **Optional - Upload Medical Documents**: You have the option to upload any relevant medical documents for a more personalized experience.
+4. **Explore the Chat**: Once registered, you can move to the chat page and  start chatting. Feel free to attach pictures in the chat for a more interactive experience.
+5. **View Alternative Products**: After viewing the generated response in the chat, you can click on the sidebar to explore alternative products.
+6. **Chat Normally**: Continue to use the chat as normal and remove pictures if needed.
+""")
+
 # Using columns to create a more dynamic layout
 col1, col2 = st.columns([2, 1])
 
@@ -29,7 +41,6 @@ with col1:
 
 with col2:
     st.image("https://source.unsplash.com/weekly?health", caption="Embark on your health journey with us!")
-
 
 # Footer
 st.markdown("---")
