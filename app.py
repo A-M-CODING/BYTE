05ede12b1e89275be8d81dadddad722371120c25
Maha Khan <129088724+mahakhan-mk@users.noreply.github.com>
2024-04-27 16:16:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("APK File Downloader")
-    st.markdown("### Download APK")
+    st.markdown("### Download APK 1")
 
     # The direct URL to the APK file in your GitHub repository
     apk_url = 'https://github.com/A-M-CODING/BYTE/raw/streamlit-flutter-apk/app-release.apk'
